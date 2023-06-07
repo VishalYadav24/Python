34ec7a49b206146d9b2451f4d75ac942c13dd72d
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-07 08:47:38 +0530

    added formatting to output result

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import urllib3
 import json
 import gzip
 import sqlite3
+from tabulate import tabulate
 http = urllib3.PoolManager()
+
+def pretty_print_data(data):
+    print(tabulate(data, headers=["Domain Code","Page Title", "View Count","Response size (bytes)"]))
 
 def fetchPagesResponse():
     # resp = http.request("GET","https://dumps.wikimedia.org/other/pageviews/2023/2023-02/pageviews-20230201-010000.gz");
@@ -13,15 +17,15 @@ def fetchPagesResponse():
         keys = ["domain_code", "page_title", "view_count", "response_size"]
         arr = []
         c = list(map(lambda x: x.split(),string_response.split('\n')))
-        for item in c:
+        for item in c[0:20]:
 
             arr.append(dict(zip(keys,item)))
 
         return arr
 
 def createDatabase():
-    conn = sqlite3.connect('pageview.db')
-    c = conn.cursor()
+    connection = sqlite3.connect('pageview.db')
+    c = connection.cursor()
     c.execute(
         '''
         CREATE TABLE IF NOT EXISTS page_data (
@@ -32,8 +36,8 @@ def createDatabase():
         )
         '''
     )
-    conn.commit()  
-    return conn
+    connection.commit()  
+    return connection
 
 def closeConnection(connectionObject):
     connectionObject.close()
@@ -43,15 +47,15 @@ def insertData(cursorObject,data):
     insert_query= "INSERT INTO page_data (domain_code, page_title, view_count,response_size) VALUES(?, ?,?,?)"
     cursorObject.execute(insert_query,data)
 def getAllRecords(cursorObject):
-    select_query= "SELECT * FROM page_data where view_count = 0"
+    select_query= "SELECT * FROM page_data"
     d = cursorObject.execute(select_query)
-    print(list(d))
+    pretty_print_data(d)
 
 def wikipedia_page_views_api():
     # fetch the data
     data = fetchPagesResponse()
     # create a connection to database
-    conn = createDatabase()
+    connection = createDatabase()
     # insert the data
     for item in data:
         a = (
@@ -61,14 +65,14 @@ def wikipedia_page_views_api():
             item["response_size"]
         )
         #print(a)
-        insertData(conn, a)
-        conn.commit()
+        insertData(connection, a)
+        connection.commit()
         
 
     # select the data from table
-    getAllRecords(conn)
+    getAllRecords(connection)
 
     #close the connection
-    closeConnection(conn)
+    closeConnection(connection)
 
 wikipedia_page_views_api()    
